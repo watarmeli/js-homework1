e4cf1ec4fe1cb818007e676ccabab349479ad661
watarmeli <watar@ukr.net>
2021-01-10 11:46:44 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function isNumberInRange(start, end, number) {
+    const isInRange = number >= start && number <= end; // дополни эту строку
+  
+    return isInRange;
+  }
+  
+  console.log(isNumberInRange(10, 30, 17));
+  console.log(isNumberInRange(10, 30, 5));
+  console.log(isNumberInRange(20, 50, 24));
+  console.log(isNumberInRange(20, 50, 76));

301a82b59e8c8a5cad5b5453682399af86bb2e3d
watarmeli <watar@ukr.net>
2021-01-10 21:27:32 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function normalizeInput(input) {
+  const normalizedInput = input.toLowerCase(); // Дополни эту строку
+  return normalizedInput;
+}
+
+console.log(normalizeInput('Привет мир'));
+console.log(normalizeInput('Это НЕ СпаМ'));
+console.log(normalizeInput('Большие СКИДКИ'));

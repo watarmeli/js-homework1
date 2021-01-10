68539db67c0e0e1a7d741ac7594d265c450bcc3a
watarmeli <watar@ukr.net>
2021-01-10 10:44:33 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function isValidPassword(password) {
+    const SAVED_PASSWORD = 'jqueryismyjam';
+    // Пиши код ниже этой строки
+    const isMatch = password === SAVED_PASSWORD;
+  
+    // Пиши код выше этой строки
+    return isMatch;
+  }
+  
+  console.log(isValidPassword('mangodab3st'));
+  console.log(isValidPassword('kiwirul3z'));
+  console.log(isValidPassword('jqueryismyjam'));

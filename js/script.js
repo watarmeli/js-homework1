e2b874432f30583a7da7bc1735e4174d87072102
watarmeli <watar@ukr.net>
2021-01-11 06:58:56 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 function checkForSpam(message) {
-  let result;
-  // Пиши код ниже этой строки
-  result = message.toLowerCase();
-  result = result.includes('spam') || result.includes('sale');
-  // Пиши код выше этой строки
-  return result;
-}
-
-console.log(checkForSpam('Latest technology news'));
-console.log(checkForSpam('JavaScript weekly newsletter'));
-console.log(checkForSpam('Get best sale offers now!'));
-console.log(checkForSpam('Amazing SalE, only tonight!'));
-console.log(checkForSpam('Trust me, this is not a spam message'));
-console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!'));
-console.log(checkForSpam('[SPAM] How to earn fast money?'));
+    let result;
+    // Пиши код ниже этой строки
+    result = message.toLowerCase();
+    result = result.includes('spam') || result.includes('sale');
+    // Пиши код выше этой строки
+    return result;
+  }
+  console.log(checkForSpam('Latest technology news'));
+  console.log(checkForSpam('JavaScript weekly newsletter'));
+  console.log(checkForSpam('Get best sale offers now!'));
+  console.log(checkForSpam('Amazing SalE, only tonight!'));
+  console.log(checkForSpam('Trust me, this is not a spam message'));
+  console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!'));
+  console.log(checkForSpam('[SPAM] How to earn fast money?'));
+  
